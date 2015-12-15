4f6fc6251e998536733d42e7624db11821d4b7aa
William Kent <wjk@sunsol.me>
2015-12-15 17:42:07 -0500

    Remove singleton mutex check
    This check no longer belongs in the wizard. A reentrancy
    check should now go in the C++ launcher, as without one the
    launcher will attempt to overwrite the files of the running
    wizard instance.

--- ChocolateyInstaller.Wizard/Program.cs
+++ ChocolateyInstaller.Wizard/Program.cs
@@ -1,7 +1,5 @@
 ﻿using System;
-using System.Threading;
 using System.Windows.Forms;
-using PSTaskDialog;
 
 namespace ChocolateyInstaller.Wizard
 {
@@ -12,36 +10,7 @@ namespace ChocolateyInstaller.Wizard
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-
-            const string LockName = "Chocolatey Installer Singleton Lock";
-
-            Mutex singletonMutex;
-            bool mutexExists = Mutex.TryOpenExisting(LockName, out singletonMutex);
-            if (mutexExists)
-            {
-                VistaTaskDialog td = new VistaTaskDialog();
-                // td.MainIcon = VistaTaskDialogIcon.Error;
-                td.MainInstruction = "Only one instance of Chocolatey Installer can run simultaneously.";
-                td.Content = "Close all other instances of Chocolatey Installer and try again.";
-                td.WindowTitle = "Chocolatey Installer";
-                td.CommonButtons = VistaTaskDialogCommonButtons.Ok;
-                td.AllowDialogCancellation = true;
-                td.Show();
-
-                singletonMutex.Close();
-                return;
-            }
-
-            try
-            {
-                singletonMutex = new Mutex(true, LockName);
-                Application.Run(new MainForm());
-            }
-            finally
-            {
-                singletonMutex.ReleaseMutex();
-                singletonMutex.Close();
-            }
+            Application.Run(new MainForm());
         }
     }
 }
